336d5e34df8f0311bee6fd3b6033ad7bddf8cb30
Fabrice Bibonne <fabrice.bibonne@insee.fr>
2024-05-27 08:03:48 +0200

    feat(new project for client side)

---
+++ metadata-clientimpl/src/main/java/poclient/TestFeign.java
@@ -0,0 +1,12 @@
+package poclient;
+
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
+
+public class TestFeign {
+
+    - Use Generated openfeign interface
+    - Configure openfeign to use okhttp client with cache activated
+    - Test
+
+}
